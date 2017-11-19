7372e942f49e99c38bddb432601c92115b3bc419
m-mueller678 <marcus.mueller.678@gmail.com>
2017-11-19 21:03:40 +0100

    process multiple render chunk updates per frame

--- src/graphics/world.rs
+++ src/graphics/world.rs
@@ -25,18 +25,22 @@ impl RenderArea {
     }
 
     fn contains(&self, pos: ChunkPos) -> bool {
-        pos.iter().enumerate().all(|(i, p)| {
-            let dif = p - self.min[i];
-            dif >= 0 && dif < self.size[i]
-        })
+        pos.iter()
+            .enumerate()
+            .all(|(i, p)| {
+                     let dif = p - self.min[i];
+                     dif >= 0 && dif < self.size[i]
+                 })
     }
 
 
     fn iter(&self) -> RenderAreaIter {
         RenderAreaIter {
             min: self.min,
-            max: [self.min[0] + self.size[0], self.min[1] + self.size[1], self.min[2] + self.size[2]],
-            pos: [self.min[0] - 1, self.min[1], self.min[2]]
+            max: [self.min[0] + self.size[0],
+                  self.min[1] + self.size[1],
+                  self.min[2] + self.size[2]],
+            pos: [self.min[0] - 1, self.min[1], self.min[2]],
         }
     }
 }
@@ -59,7 +63,7 @@ impl Iterator for RenderAreaIter {
                 self.pos[1] = self.min[1];
                 self.pos[2] += 1;
                 if self.pos[2] == self.max[2] {
-                    return None
+                    return None;
                 }
             }
         }
@@ -84,11 +88,11 @@ impl WorldRender {
         let player_pos_init = ChunkPos([i32::min_value(); 3]);
         let (send, rec) = channel();
         let shared = Arc::new(Mutex::new(RenderArea::new()));
-        let shared2 = shared.clone();
+        let shared2 = Arc::clone(&shared);
         thread::spawn(move || {
-            let updater = RenderChunkUpdater::new(shared2, world, send);
-            updater.run();
-        });
+                          let updater = RenderChunkUpdater::new(shared2, world, send);
+                          updater.run();
+                      });
         WorldRender {
             render_dist: 4,
             shared_area: shared,
@@ -103,7 +107,7 @@ impl WorldRender {
         transform: [[f32; 4]; 4],
         sampler: glium::uniforms::Sampler<CompressedSrgbTexture2dArray>,
         quad_shader: &glium::Program,
-    ) -> Result<(), glium::DrawError> {
+) -> Result<(), glium::DrawError>{
         let params = glium::DrawParameters {
             depth: glium::Depth {
                 test: glium::draw_parameters::DepthTest::IfLess,
@@ -121,68 +125,69 @@ impl WorldRender {
         let chunk_iter = self.render_chunks
             .iter()
             .filter_map(|opt| {
-                opt.as_ref().and_then(|&(pos, ref chunk)| {
-                    let corners: Vec<[f32; 3]> = CORNER_OFFSET
-                        .iter()
-                        .map(|c| {
-                            [c[0] + pos[0] as f32,
-                                c[1] + pos[1] as f32,
-                                c[2] + pos[2] as f32]
-                        })
-                        .map(|c| vec3_scale(c, CHUNK_SIZE as f32))
-                        .map(|c| col_mat4_transform(transform, [c[0], c[1], c[2], 1.]))
-                        .map(|c| [c[0] / c[3], c[1] / c[3], c[2] / c[3]])
-                        .collect();
-                    let visible = corners.iter().any(|c| c[2] < 1.) && corners.iter().any(|c| c[0] < 1.) &&
-                        corners.iter().any(|c| c[0] > -1.) &&
-                        corners.iter().any(|c| c[1] < 1.) &&
-                        corners.iter().any(|c| c[1] > -1.);
-                    if visible {
-                        Some(chunk)
-                    } else {
-                        None
-                    }
-                })
+                opt.as_ref()
+                    .and_then(|&(pos, ref chunk)| {
+                        let corners: Vec<[f32; 3]> = CORNER_OFFSET
+                            .iter()
+                            .map(|c| {
+                                     [c[0] + pos[0] as f32,
+                                      c[1] + pos[1] as f32,
+                                      c[2] + pos[2] as f32]
+                                 })
+                            .map(|c| vec3_scale(c, CHUNK_SIZE as f32))
+                            .map(|c| col_mat4_transform(transform, [c[0], c[1], c[2], 1.]))
+                            .map(|c| [c[0] / c[3], c[1] / c[3], c[2] / c[3]])
+                            .collect();
+                        let visible = corners.iter().any(|c| c[2] < 1.) &&
+                                      corners.iter().any(|c| c[0] < 1.) &&
+                                      corners.iter().any(|c| c[0] > -1.) &&
+                                      corners.iter().any(|c| c[1] < 1.) &&
+                                      corners.iter().any(|c| c[1] > -1.);
+                        if visible { Some(chunk) } else { None }
+                    })
             });
         for chunk in chunk_iter {
             chunk.draw(surface, &uniforms, &params, quad_shader)?;
         }
         Ok(())
     }
-    pub fn update<F: glium::backend::Facade>(&mut self,
-                                             player_pos: BlockPos,
-                                             facade: &F) {
+    pub fn update<F: glium::backend::Facade>(&mut self, player_pos: BlockPos, facade: &F) {
         let chunk_pos = player_pos.containing_chunk();
         if chunk_pos != self.player_chunk {
             self.player_chunk = chunk_pos;
             *self.shared_area.lock().unwrap() = Self::render_area(self.render_dist, chunk_pos);
         }
-        match self.update_receiver.try_recv() {
-            Ok((index, update)) => {
-                if self.render_chunks.len() <= index {
-                    self.render_chunks.resize_default(index + 1)
-                }
-                if let Some(update) = update {
-                    if update.1.is_empty() {
-                        self.render_chunks[index] = None;
-                    } else {
-                        match &mut self.render_chunks[index] {
-                            &mut Some((ref mut pos, ref mut chunk)) => {
-                                *pos = update.0;
-                                chunk.apply_update(update.1);
-                            }
-                            none => {
-                                *none = Some((update.0, RenderChunk::from_update(facade, update.1)));
+        loop {
+            match self.update_receiver.try_recv() {
+                Ok((index, update)) => {
+                    if self.render_chunks.len() <= index {
+                        self.render_chunks.resize_default(index + 1)
+                    }
+                    if let Some(update) = update {
+                        if update.1.is_empty() {
+                            self.render_chunks[index] = None;
+                        } else {
+                            match &mut self.render_chunks[index] {
+                                &mut Some((ref mut pos, ref mut chunk)) => {
+                                    *pos = update.0;
+                                    chunk.apply_update(update.1);
+                                }
+                                none => {
+                                    *none = Some((update.0,
+                                                  RenderChunk::from_update(facade, update.1)));
+                                }
                             }
                         }
+                    } else {
+                        self.render_chunks[index] = None;
                     }
-                } else {
-                    self.render_chunks[index] = None;
                 }
-            }
-            Err(TryRecvError::Empty) => {},
-            Err(TryRecvError::Disconnected) => {
-                panic!("connection to Render chunk updater lost");
+                Err(TryRecvError::Empty) => {
+                    break;
+                }
+                Err(TryRecvError::Disconnected) => {
+                    panic!("connection to Render chunk updater lost");
+                }
             }
         }
     }
@@ -201,21 +206,21 @@ struct RenderChunkUpdater {
     chunk_indices: HashMap<ChunkPos, usize>,
     render_area: RenderArea,
     world: Arc<World>,
-    update_sender: Sender<UpdateMessage>
+    update_sender: Sender<UpdateMessage>,
 }
 
 impl RenderChunkUpdater {
-    fn new(
-        area: Arc<Mutex<RenderArea>>,
-        world: Arc<World>,
-        update_sender: Sender<UpdateMessage>) -> Self {
+    fn new(area: Arc<Mutex<RenderArea>>,
+           world: Arc<World>,
+           update_sender: Sender<UpdateMessage>)
+           -> Self {
         RenderChunkUpdater {
             shared_area: area,
             is_used: vec![false; DEFAULT_RENDER_CHUNK_COUNT],
             chunk_indices: HashMap::new(),
             render_area: RenderArea::new(),
             world,
-            update_sender: update_sender
+            update_sender: update_sender,
         }
     }
     fn run(mut self) {
@@ -223,7 +228,7 @@ impl RenderChunkUpdater {
             if Arc::strong_count(&self.shared_area) == 1 {
                 return;
             }
-            let new_render_area ={
+            let new_render_area = {
                 *self.shared_area.lock().unwrap()
             };
             if new_render_area != self.render_area {
@@ -233,7 +238,9 @@ impl RenderChunkUpdater {
             if let Some(pos) = world.poll_chunk_update() {
                 if let Some(&index) = self.chunk_indices.get(&pos) {
                     let update = RenderChunkUpdate::new(&*world, pos);
-                    self.update_sender.send((index, Some((pos, update)))).unwrap();
+                    self.update_sender
+                        .send((index, Some((pos, update))))
+                        .unwrap();
                 }
             } else {
                 thread::sleep(Duration::from_millis(5));
@@ -242,24 +249,22 @@ impl RenderChunkUpdater {
     }
     fn update_area(&mut self, new_area: RenderArea) {
         let &mut RenderChunkUpdater {
-            ref mut is_used,
-            ref mut chunk_indices,
-            ref mut render_area,
-            ref mut world,
-            ref mut update_sender,
-            ..
-        } = self;
+                     ref mut is_used,
+                     ref mut chunk_indices,
+                     ref mut render_area,
+                     ref mut world,
+                     ref mut update_sender,
+                     ..
+                 } = self;
         *render_area = new_area;
         {
-            chunk_indices.retain(|&pos, &mut index| {
-                if new_area.contains(pos) {
-                    true
-                } else {
-                    update_sender.send((index, None)).unwrap();
-                    is_used[index] = false;
-                    false
-                }
-            });
+            chunk_indices.retain(|&pos, &mut index| if new_area.contains(pos) {
+                                     true
+                                 } else {
+                                     update_sender.send((index, None)).unwrap();
+                                     is_used[index] = false;
+                                     false
+                                 });
         }
         let world = world.read();
         for pos in new_area.iter() {
@@ -267,12 +272,16 @@ impl RenderChunkUpdater {
                 if let Some(empty) = (0..is_used.len()).find(|&i| !is_used[i]) {
                     is_used[empty] = true;
                     v.insert(empty);
-                    update_sender.send((empty, Some((pos, RenderChunkUpdate::new(&*world, pos))))).unwrap();
+                    update_sender
+                        .send((empty, Some((pos, RenderChunkUpdate::new(&*world, pos)))))
+                        .unwrap();
                 } else {
                     let index = is_used.len();
                     is_used.push(false);
                     v.insert(index);
-                    update_sender.send((index, Some((pos, RenderChunkUpdate::new(&*world, pos))))).unwrap();
+                    update_sender
+                        .send((index, Some((pos, RenderChunkUpdate::new(&*world, pos)))))
+                        .unwrap();
                     return;
                 }
             };
